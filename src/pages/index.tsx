61a8ed752f70003d739884f0ecc4f13fc5e14678
Paul Mourer <paul.mourer@gmail.com>
2025-04-21 07:10:18 -0600

    Page title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import { Strava } from "@/logos/strava-logo";
 import { GitHub } from "@/logos/github-logo";
 import { LinkedIn } from "@/logos/linkedin-logo";
 import { Contact } from "@/misc/contact";
+import Head from "next/head";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -17,38 +18,45 @@ const geistMono = Geist_Mono({
 
 export default function Home() {
   return (
-    <div
-      className={`${geistSans.className} ${geistMono.className} flex flex-col items-center justify-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-platinum text-berkeley-blue`}
-    >
-      <main className="flex flex-col gap-4 row-start-2 items-center">
-        <div className="flex flex-col gap-1 items-center">
-          <h1 className="font-bold text-3xl">Paul Mourer</h1>
-          <p>Leadville, CO</p>
-        </div>
-        <div className="text-center text-jet">
-          <p>software engineer</p>
-          <p>tinkerer</p>
-          <p>maker</p>
-          <p>cyclist</p>
-        </div>
-        <div className="text-burnt-orange flex gap-4">
-          <a href="https://www.github.com/pm0u">
-            <GitHub className="h-4 w-auto hover:brightness-75 transition-all duration-200" />
-          </a>
-          <a href="https://www.linkedin.com/in/pmou">
-            <LinkedIn className="h-4 w-auto hover:brightness-75 transition-all duration-200" />
-          </a>
-          <a href="https://www.printables.com/@PaulMourer_2841132">
-            <Printables className="h-4 w-auto hover:brightness-75 transition-all duration-200" />
-          </a>
-          <a href="https://www.strava.com/athletes/6217227">
-            <Strava className="h-4 w-auto hover:brightness-75 transition-all duration-200" />
-          </a>
-        </div>
-        <div>
-          <Contact />
-        </div>
-      </main>
-    </div>
+    <>
+      <Head>
+        <title>
+          paul mourer | software engineer - tinkerer - maker - cyclist
+        </title>
+      </Head>
+      <div
+        className={`${geistSans.className} ${geistMono.className} flex flex-col items-center justify-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-platinum text-berkeley-blue`}
+      >
+        <main className="flex flex-col gap-4 row-start-2 items-center">
+          <div className="flex flex-col gap-1 items-center">
+            <h1 className="font-bold text-3xl">paul mourer</h1>
+            <p>leadville, co</p>
+          </div>
+          <div className="text-center text-jet">
+            <p>software engineer</p>
+            <p>tinkerer</p>
+            <p>maker</p>
+            <p>cyclist</p>
+          </div>
+          <div className="text-burnt-orange flex gap-4">
+            <a href="https://www.github.com/pm0u">
+              <GitHub className="h-4 w-auto hover:brightness-75 transition-all duration-200" />
+            </a>
+            <a href="https://www.linkedin.com/in/pmou">
+              <LinkedIn className="h-4 w-auto hover:brightness-75 transition-all duration-200" />
+            </a>
+            <a href="https://www.printables.com/@PaulMourer_2841132">
+              <Printables className="h-4 w-auto hover:brightness-75 transition-all duration-200" />
+            </a>
+            <a href="https://www.strava.com/athletes/6217227">
+              <Strava className="h-4 w-auto hover:brightness-75 transition-all duration-200" />
+            </a>
+          </div>
+          <div>
+            <Contact />
+          </div>
+        </main>
+      </div>
+    </>
   );
 }
